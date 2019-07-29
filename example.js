fefb89f7940de9129557aee62adf6fcabbd1fb90
Muin Syed <mhc.msyed@gmail.com>
2019-07-29 16:54:20 -0400

    Added spacing to test to see if raw js file will preserve spaces and comments and indents

--- example.js
+++ example.js
@@ -4,3 +4,8 @@ x = 'new value';
 console.log(y);
 y = 'another new value';
 console.log(y);
+           //Does spacing and commenting work?
+           {
+           	Object: 'yo'
+           };
+const x = 9;
