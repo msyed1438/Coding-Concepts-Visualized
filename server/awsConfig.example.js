81a3a74ebf61a9bf77d76e5141b0a4dba9e66f52
Muin Syed <mhc.msyed@gmail.com>
2020-01-15 21:35:48 -0500

    Edited the example config file to also include the bucket name in its module

--- server/awsConfig.example.js
+++ server/awsConfig.example.js
@@ -1,7 +1,8 @@
-module.exports.accessKey = '<Access key from S3 goes here>';
-module.exports.secretkey = '<Secret key from S3 goes here>';
-
 /* Go to https://docs.aws.amazon.com/general/latest/gr/aws-sec-cred-types.html 
 to set up your Amazon S3 credentials. If S3 is not working with this app, the 
 index.js file needs to be refactored with a new storage option, such as Google Cloud 
 Services, OneDrive, DropBox, etc. */
+
+module.exports.S3_BUCKET = '<Name of the S3 bucket goes here>';
+module.exports.accessKey = '<Access key from S3 goes here>';
+module.exports.secretkey = '<Secret key from S3 goes here>';
